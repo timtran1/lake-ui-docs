8f88d2bc6e33e0e114bf95d78aefe4fcfa2747c0
Tim Tran <tim.tran.201@gmail.com>
2023-02-27 12:06:57 +0700

    update

--- src/routes/InputPage.jsx
+++ src/routes/InputPage.jsx
@@ -137,10 +137,10 @@ function MyComponent() {
                 </p>
 
                 <ExampleCodeSection>
-                    <div className={`block`}>
-                        <div>
-                            <Input className="border border-gray-400 mr-2 mb-2" placeholder="Input with border"/>
-                            <Input className="rounded-none mb-2" placeholder="Square input"/>
+                    <div className="max-w-lg py-6">
+                        <div className="flex space-x-2">
+                            <Input className="mb-2" placeholder="Normal input"/>
+                            <Input className="border border-gray-400 mb-2" placeholder="Input with border"/>
                         </div>
                         <div>
                             <Input className="w-[100px] mr-2 mb-2" placeholder="Short input"/>
@@ -149,13 +149,25 @@ function MyComponent() {
                         <div className="mb-2">
                             <Input className="w-full" placeholder="Full width input"/>
                         </div>
+
+                        <div className="flex space-x-2">
+                            <Input className="w-1/2 rounded-full mb-2" placeholder="Rounded input"/>
+                            <Input className="w-1/2 rounded-none mb-2" placeholder="Square input"/>
+                        </div>
+
+                        <div className="flex space-x-2">
+                            <Input className="w-1/3 bg-yellow-300 mb-2" placeholder="Yellow background"/>
+                            <Input className="w-1/3 bg-lime-400 mb-2" placeholder="Green background"/>
+                            <Input className="w-1/3 bg-red-300 mb-2" placeholder="Red background"/>
+                        </div>
+
+                        <div className="flex flex-col">
+                            <Input className="h-7 mb-2 text-sm" placeholder="Small"/>
+                            <Input className="mb-2" placeholder="Normal"/>
+                            <Input className="h-18 text-3xl" placeholder="Large"/>
+                        </div>
                     </div>
                 </ExampleCodeSection>
-
-                {/*styling*/}
-                border color, border radius, bg color, sizes
-
-                {/*data binding*/}
 
                 {/*enable focus effect*/}
 
