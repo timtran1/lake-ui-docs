66acab5d81b457b9efd438e1540eaa66bd4e43bc
Tim Tran <tim.tran.201@gmail.com>
2023-03-28 16:09:48 +0700

    update

--- src/routes/NavbarPage.jsx
+++ src/routes/NavbarPage.jsx
@@ -191,11 +191,111 @@ export default function () {
 
                 <h3 className="text-lg font-semibold mt-10">TopBar + SideDrawer</h3>
                 <p className="mb-4">
-                    To use the TopBar and SideDrawer together, import the TopBar and SideDrawer components and add them to your JSX code.
+                    To use the TopBar and SideDrawer together, import the TopBar and SideDrawer components and add them
+                    to your JSX code.
                 </p>
                 <ExampleCodeSection>
+                    <div className={`w-[500px] bg-white my-10`}>
+                        <TopBar className="flex justify-between items-center">
+                            <div className="flex items-center">
+                                <Button hoverEffect={false} className={`p-2 pr-4 bg-transparent text-black`}
+                                        onClick={() => setSidebarOpen2(true)}>
+                                    <FontAwesomeIcon icon={faBars}/>
+                                </Button>
+                                <div className="flex">
+                                    <img src="/logo.png" className="h-8 rounded shadow"/>
+                                    <h1 className={`font-bold text-2xl ml-2`}>Lake UI</h1>
+                                </div>
+                            </div>
 
+
+                            <a href="https://github.com/timtran1/lake-ui" target="_blank" className="block">
+                                <img className="h-5 mr-1.5" src="/github.png"/>
+                            </a>
+                        </TopBar>
+
+                        <main className="p-3 h-[400px] overflow-y-auto">
+                            <h2 className="font-semibold">The standard Lorem Ipsum passage, used since the 1500s</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                                ut
+                                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                                ullamco
+                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+                                in
+                                voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+                                laborum.</p>
+                            <br/>
+                            <h2 className="font-semibold">Section 1.10.32 of "de Finibus Bonorum et Malorum", written by
+                                Cicero in 45 BC</h2>
+                            <p>
+                                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                                laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+                                architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+                                sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+                                voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit
+                                amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut
+                                labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
+                                nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+                                consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
+                                nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
+                                pariatur?
+                            </p>
+                        </main>
+                    </div>
                 </ExampleCodeSection>
+
+                <h3 className="text-lg font-semibold mt-8">TopBar Props</h3>
+                <table className={`w-full mt-2`}>
+                    <thead className={`text-left`}>
+                    <tr>
+                        <th>Prop name</th>
+                        <th>Type</th>
+                        <th>Default value</th>
+                        <th>Description</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td><code>...other</code></td>
+                        <td><code>any</code></td>
+                        <td></td>
+                        <td>Any additional props passed to the component will be spread here.</td>
+                    </tr>
+                    </tbody>
+                </table>
+
+                <h3 className="text-lg font-semibold mt-8">SideDrawer Props</h3>
+                <table className={`w-full mt-2`}>
+                    <thead className={`text-left`}>
+                    <tr>
+                        <th>Prop name</th>
+                        <th>Type</th>
+                        <th>Default value</th>
+                        <th>Description</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td><code>useOpen</code></td>
+                        <td><code>array</code></td>
+                        <td><code></code></td>
+                        <td>React <code>useState</code> hook array for the open state of the SideDrawer.</td>
+                    </tr>
+                    <tr>
+                        <td><code>right</code></td>
+                        <td><code>boolean</code></td>
+                        <td>false</td>
+                        <td>Places the SideDrawer on the right side.</td>
+                    </tr>
+                    <tr>
+                        <td><code>...other</code></td>
+                        <td><code>any</code></td>
+                        <td></td>
+                        <td>Any additional props passed to the component will be spread here.</td>
+                    </tr>
+                    </tbody>
+                </table>
             </main>
         </div>
     );
