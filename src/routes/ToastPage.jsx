cd052d491293cd5be624d3260e256fc951ad3f2c
Tim Tran <tim.tran.201@gmail.com>
2023-03-28 21:57:52 +0700

    update

--- src/routes/ToastPage.jsx
+++ src/routes/ToastPage.jsx
@@ -8,6 +8,7 @@ import {
 // import Toast from "../components/base/Toast.jsx";
 import SimpleCodeSection from "../components/SimpleCodeSection.jsx";
 import ExampleCodeSection from "../components/ExampleCodeSection.jsx";
+import Code from "../components/Code.jsx";
 
 
 export default function () {
@@ -17,6 +18,11 @@ export default function () {
     const [toastLeftOpen, setToastLeftOpen] = useState(false)
     const [toastBottomOpen, setToastBottomOpen] = useState(false)
     const [toastLeftBottomOpen, setToastLeftBottomOpen] = useState(false)
+
+    const [toastInfoOpen, setToastInfoOpen] = useState(false)
+    const [toastSuccessOpen, setToastSuccessOpen] = useState(false)
+    const [toastWarningOpen, setToastWarningOpen] = useState(false)
+    const [toastErrorOpen, setToastErrorOpen] = useState(false)
 
     const [toastCustomDelayOpen, setToastCustomDelayOpen] = useState(false)
     const [toastDisableAutoOpen, setToastDisableAutoOpen] = useState(false)
@@ -75,7 +81,33 @@ export default function () {
 
                 <h3 className="text-lg font-semibold mt-6">Custom colors and styling</h3>
                 <ExampleCodeSection>
+                    <div className="flex flex-col gap-6 my-10">
+                        <Button onClick={() => setToastInfoOpen(true)} className="bg-blue-500">
+                            Open an info Toast
+                        </Button>
+                        <Button onClick={() => setToastSuccessOpen(true)} className="bg-green-500">
+                            Open a success Toast
+                        </Button>
+                        <Button onClick={() => setToastWarningOpen(true)} className="bg-yellow-500">
+                            Open a warning Toast
+                        </Button>
+                        <Button onClick={() => setToastErrorOpen(true)} className="bg-red-500">
+                            Open an error Toast
+                        </Button>
 
+                        <Toast useOpen={[toastInfoOpen, setToastInfoOpen]} className="bg-blue-500">
+                            You opened an info toast!
+                        </Toast>
+                        <Toast useOpen={[toastSuccessOpen, setToastSuccessOpen]} className="bg-green-500">
+                            You opened a success toast!
+                        </Toast>
+                        <Toast useOpen={[toastWarningOpen, setToastWarningOpen]} className="bg-yellow-500">
+                            You opened a warning toast!
+                        </Toast>
+                        <Toast useOpen={[toastErrorOpen, setToastErrorOpen]} className="bg-red-500">
+                            You opened an error toast!
+                        </Toast>
+                    </div>
                 </ExampleCodeSection>
 
                 <h3 className="text-lg font-semibold mt-6">Custom timeout and disabling auto-close</h3>
@@ -111,16 +143,80 @@ export default function () {
                         <Toast useOpen={[toastDisableCloseBtn, setToastDisableCloseBtn]} disableCloseBtn>
                             <p>This toast has no close button.</p>
                         </Toast>
-                        <Toast useOpen={[toastCustomButtonOpen, setToastCustomButtonOpen]} disableCloseBtn
-                               timeOut={90000}>
+                        <Toast useOpen={[toastCustomButtonOpen, setToastCustomButtonOpen]} disableCloseBtn>
                             <span>This toast has a custom close button.</span>
-                            <Button className={`bg-black py-1 px-2 ml-1 my-1`}
+                            <Button className="bg-gray-200 text-blue-500 py-1 px-2 ml-2 my-1"
                                     onClick={() => setToastCustomButtonOpen(false)}>
                                 Close
                             </Button>
                         </Toast>
                     </div>
                 </SimpleCodeSection>
+
+
+                <h3 className="text-lg font-semibold mt-8">Props</h3>
+                <table className={`w-full mt-2`}>
+                    <thead className={`text-left`}>
+                    <tr>
+                        <th>Prop name</th>
+                        <th>Type</th>
+                        <th>Default value</th>
+                        <th>Required</th>
+                        <th>Description</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td><code>useOpen</code></td>
+                        <td><code>array</code></td>
+                        <td><code></code></td>
+                        <td>yes</td>
+                        <td>React <Code>useState</Code> hook array for the current tab index, an integer.</td>
+                    </tr>
+                    <tr>
+                        <td><code>timeOut</code></td>
+                        <td><code>integer</code></td>
+                        <td><code>3000</code></td>
+                        <td></td>
+                        <td>Time in milliseconds before the toast will auto-dismiss.</td>
+                    </tr>
+                    <tr>
+                        <td><code>left</code></td>
+                        <td><code>boolean</code></td>
+                        <td><code>false</code></td>
+                        <td></td>
+                        <td>Whether the toast should be positioned on the left side of the screen.</td>
+                    </tr>
+                    <tr>
+                        <td><code>bottom</code></td>
+                        <td><code>boolean</code></td>
+                        <td><code>false</code></td>
+                        <td></td>
+                        <td>Whether the toast should be positioned at the bottom of the screen.</td>
+                    </tr>
+                    <tr>
+                        <td><code>disableAutoClose</code></td>
+                        <td><code>boolean</code></td>
+                        <td><code>false</code></td>
+                        <td></td>
+                        <td>Disable auto-dismiss.</td>
+                    </tr>
+                    <tr>
+                        <td><code>disableCloseBtn</code></td>
+                        <td><code>boolean</code></td>
+                        <td><code>false</code></td>
+                        <td></td>
+                        <td>Disable the default close button.</td>
+                    </tr>
+                    <tr>
+                        <td><code>...other</code></td>
+                        <td><code>any</code></td>
+                        <td></td>
+                        <td></td>
+                        <td>Any additional props passed to the component will be spread here.</td>
+                    </tr>
+                    </tbody>
+                </table>
 
 
             </main>
